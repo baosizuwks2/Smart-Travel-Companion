341a682f807d737ebbaaf6424a421bd461a5a1d6
Bato <buibao32045@gmail.com>
2025-04-10 10:46:09 +0700

    Them 2 bang Destinations and SavedDestinations voi migration

---
+++ SmartTravelCompanion/Migrations/202504100343419_AddDestinationsAndSavedDestinations.cs
@@ -0,0 +1,43 @@
+﻿namespace SmartTravelCompanion.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDestinationsAndSavedDestinations : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Destinations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                        UniqueReason = c.String(),
+                        SafetyTips = c.String(),
+                        ActivityType = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.SavedDestinations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        DestinationId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.UserPreferences", "PreferredActivities", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.UserPreferences", "PreferredActivities");
+            DropTable("dbo.SavedDestinations");
+            DropTable("dbo.Destinations");
+        }
+    }
+}
--- SmartTravelCompanion/Models/TravelDbContext.cs
+++ SmartTravelCompanion/Models/TravelDbContext.cs
@@ -9,11 +9,18 @@ namespace SmartTravelCompanion.Models
 {
     public class TravelDbContext : DbContext
     {
-        public TravelDbContext() : base("name=TravelDbConnection")
-        {
-        }
+        public TravelDbContext() : base("name=TravelDbConnection") { }
 
         public DbSet<Deal> Deals { get; set; }
         public DbSet<UserPreference> UserPreferences { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<SavedDestination> SavedDestinations { get; set; }
+    }
+
+    public class SavedDestination
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int DestinationId { get; set; }
     }
 }
